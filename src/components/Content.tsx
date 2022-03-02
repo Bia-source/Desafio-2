51a87477c048a257ea39f4cb34c76b61d53ef257
Bia-source <bia_ferreirads@yahoo.com>
2022-03-02 15:08:16 -0300

    desafio 2

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,54 @@
-export function Content() {
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps{
+    selectedGenreId: number;
+    selectedGenreTitle: string;
+}
+
+
+export function Content({ selectedGenreId, selectedGenreTitle }:ContentProps) {
   // Complete aqui
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+  }, [selectedGenreId]);
+    
+  return (
+    <div className="container">
+      <header>
+          <span className="category">Categoria:<span> {selectedGenreTitle}</span></span>
+        </header>
+        <main>
+          <div className="movies-list">
+            {movies.map((item) => {
+                 return (
+                  <MovieCard 
+                  key ={item.imdbID} 
+                  title={item.Title} 
+                  poster={item.Poster} 
+                  runtime={item.Runtime} 
+                  rating={item.Ratings[0].Value} 
+               />
+                 )
+            })}
+          </div>
+        </main>
+    </div>
+  )
 }
